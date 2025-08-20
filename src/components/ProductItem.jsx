9c784cc2e39ca25d8a5b31465be75139c2d115bd
Violetta Lezhnenko <leznenkovioletta@gmail.com>
2025-08-20 13:43:58 +0300

    add product list with data from api

---
+++ src/components/ProductItem.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+
+const ProductItem = ({ product }) => {
+  return (
+    <div className="p-4 rounded-xl flex flex-col justify-between w-[32%] shadow-2xl">
+      <div className="flex flex-col">
+        <img
+          src={product.picture}
+          alt={product.title}
+          className=" w-full h-48 object-cover mb-4 rounded"
+        />
+        <div></div>
+        <h2 className=" text-lg font-semibold mb-2 font-header">
+          {product.title}
+        </h2>
+        <p className=" text-sm text-gray-500 mb-2">Brand: {product.brand}</p>
+        <p className=" text-sm text-gray-400 mb-2">
+          Category: {product.category}
+        </p>
+        <p className=" text-sm mb-2">{product.description}</p>
+        <p className=" text-lg font-bold mb-2">${product.price}</p>
+        <p className=" text-sm text-yellow-500">Rating: {product.rating} ⭐</p>
+      </div>
+
+      <button className=" mt-4 bg-basic-red text-white py-2 px-4 rounded hover:bg-basic-black">
+        Add to Cart
+      </button>
+    </div>
+  );
+};
+
+export default ProductItem;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,50 @@
+import ProductItem from "../components/ProductItem";
+import { useState, useEffect } from "react";
+import axios from "axios";
+
 function Home({ data }) {
+  const [products, setProducts] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  const fetchProducts = async () => {
+    const api_url =
+      "https://68a58ad62a3deed2960dd7bd.mockapi.io/api/v1/products";
+    const res = await axios.get(api_url);
+    console.log(res.data);
+    return res.data;
+  };
+  useEffect(() => {
+    const getProduct = async () => {
+      try {
+        setLoading(true);
+        const prods = await fetchProducts();
+        setProducts(prods || []);
+        console.log(prods);
+        console.log(products);
+      } catch (err) {
+        console.log("Error:" + err);
+        return [];
+      } finally {
+        setLoading(false);
+      }
+    };
+    getProduct();
+  }, []);
+
   return (
-    <div className="flex bg-basic-weight w-full min-h-[85vh]">
-      <section className="bg-dark-weight flex-1">
-        <h2>Filters</h2>
+    <div className="flex bg-basic-weight w-full min-h-[85vh] rounded-xl">
+      <section className="bg-dark-weight flex-1 py-5 px-5 rounded-xl">
+        <h2 className="font-header text-xl font-bold">Sorting</h2>
+        <h2 className="font-header text-xl font-bold">Filters</h2>
       </section>
-      <div className="flex-4">
-        <h2>Home</h2>
+      <div className="flex-4 py-5 px-10 flex flex-wrap justify-between gap-2">
+        {loading ? (
+          <div>Loading products...</div>
+        ) : products.length > 0 ? (
+          products.map((pr) => <ProductItem product={pr} key={pr.id}/>)
+        ) : (
+          <div>No products available.</div>
+        )}
       </div>
     </div>
   );
